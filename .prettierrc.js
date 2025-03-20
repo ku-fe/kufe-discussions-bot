c4f3fa7afc67917cf708b6a3060f9689dc0d66b2
shinwonse <sinwonse@gmail.com>
2025-03-20 22:46:02 +0900

    Add Prettier and ESLint configuration files, update package.json with new dependencies, and simplify tsconfig by extending from @wonse/tsconfig.

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+export default {
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  arrowParens: "always",
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+};
---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+import wonseEslintTs from '@wonse/eslint-ts';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig({
+  plugins: [wonseEslintTs],
+});
